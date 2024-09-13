8ec718bccb46d59b395224b61f7c168bd664b418
Kuznec77 <super.kuznec-77@yandex.ru>
2024-09-13 19:32:33 +0300

    13.09

---
+++ main.py
@@ -0,0 +1,5 @@
+
+print  (9 ** 0.5 * 5) , # "1St program"
+print (9.99 > 9.98) , (1000 != 1000.1) , # "2nd program"
+print  ((2*2+2)==(2*(2+2))) , # "3rd program"
+print  (int(123.456*10)%10) , #4th program
