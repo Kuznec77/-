5d63e1f8647e0f14f274456222a4ffb255cea9e7
Sony <super.kuznec-77@yandex.ru>
2024-09-21 13:44:37 +0300

    21/09

---
+++ main1.py
@@ -0,0 +1,6 @@
+example='Топинамбур'
+print(example [0])
+print(example [-1])
+print(example [5:])
+print(example [:: -1])
+print((example [1]),(example [3]),(example [5]),(example [7]),(example [9]))
