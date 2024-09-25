762db17a4fa68390859ba7fde3a6a28af9bead66
Sony <super.kuznec-77@yandex.ru>
2024-09-25 18:43:13 +0300

    25/09

---
+++ module_1_5.py
@@ -0,0 +1,7 @@
+immutable_var = (1,2,'a', 'b' )
+print(immutable_var)
+ # immutable_var[0]='banana'
+# Кортеж не поддерживает обращение по эллементам
+mutable_list = ['fish', 'burger', 'orange',]
+mutable_list[1] = 'apple'
+print(mutable_list)
