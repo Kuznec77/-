01784fdeedad2c4f829e3a13814222d370ff6188
Sony <super.kuznec-77@yandex.ru>
2024-09-29 20:45:17 +0300

    29/09/2

---
+++ main5.py
@@ -0,0 +1,13 @@
+def get_matrix(n, m, value):
+    matrix = []
+    for i in range(n):
+        matrix.append([])
+        for j in range(m):
+            matrix[i].append(value)
+    return matrix
+result1 = get_matrix(2, 2, 10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
+print(result1)
+print(result2)
+print(result3)
