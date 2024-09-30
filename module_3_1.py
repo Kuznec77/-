5d56c3095b825cd3c09e9e67c6ca347c28ef16fa
Sony <super.kuznec-77@yandex.ru>
2024-09-30 19:26:14 +0300

    30/09/1

---
+++ module_3_1.py
@@ -0,0 +1,26 @@
+calls = 0
+def count_calls():
+    global calls
+    calls += 1
+def string_info(string):
+    stroka = str(string)
+    result = (len(stroka), stroka.upper(), stroka.lower())
+    count_calls()
+    return result
+def is_contains(string, list_to_search):
+    string = str(string).lower()
+    list_to_search = list(list_to_search)
+    count_calls()
+    for i in range(len(list_to_search)):
+        if str(list_to_search[i]).lower() == string:
+            result = True
+            break
+        else:
+            result = False
+            continue
+    return result
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(calls)
