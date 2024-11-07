066a8f7fc7687a518ebf4ee1cd375332f39afa28
Kuznec77 <super.kuznec-77@yandex.ru>
2024-11-07 20:52:19 +0300

    Update module5hard.py

--- module5hard.py
+++ module5hard.py
@@ -97,9 +97,7 @@ print(ur.get_videos('ПРОГ'))
 # # Проверка на вход пользователя и возрастное ограничение
 ur.watch_video('Для чего девушкам парень программист?')
 ur.register('vasya_pupkin', 'lolkekcheburek', 13)
-ur.watch_video('Для чего девушкам парень программист?')
 ur.register('urban_pythonist', 'iScX4vIJClb9YQavjAgF', 25)
-ur.watch_video('Для чего девушкам парень программист?')
 
 # Проверка входа в другой аккаунт
 ur.register('vasya_pupkin', 'F8098FM8fjm9jmi', 55)
