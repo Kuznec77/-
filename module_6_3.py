cf2aa3ed83396b2a88282dd95b29cdb5d0b99ea5
Kuznec77 <super.kuznec-77@yandex.ru>
2024-11-29 18:49:43 +0300

    Update module_6_3.py

--- module_6_3.py
+++ module_6_3.py
@@ -1,34 +1,32 @@
-class Horse:            # класс описывающий лошадь.
-    x_distance = 0      # пройденный путь.
-    _sound = 'Frrr'     # звук, который издаёт лошадь.
+class Horse:            
+    x_distance = 0      
+    _sound = 'Frrr'     
 
-    def run(self, dx):  # dx - изменение дистанции, увеличивает x_distance на dx
+    def run(self, dx):  
         self.x_distance += dx
 
-class Eagle:            # класс описывающий орла
-    y_distance = 0      # высота полёта
-    sound = 'I train, eat, sleep, and repeat' # звук, который издаёт орёл (отсылка)
+class Eagle:            
+    y_distance = 0      
+    sound = 'I train, eat, sleep, and repeat' 
 
-    def fly(self, dy):  # где dy - изменение дистанции, увеличивает y_distance на dy.
+    def fly(self, dy):  
         self.y_distance += dy
 
-class Pegasus(Horse, Eagle):  # класс описывающий пегаса. Наследуется от Horse и Eagle в том же порядке.
-# Объект такого класса должен обладать атрибутами классов родителей в порядке наследования.
-# Также обладает методами:
+class Pegasus(Horse, Eagle):  
 
     def __init__(self):
          self.sound = super()._sound
          self.sound = super().sound
 
-    def move(self, dx, dy):   #- где dx и dy изменения дистанции.
+    def move(self, dx, dy):   
         self.run(dx)
         self.fly(dy)
 
-    def get_pos(self):  # возвращает текущее положение пегаса в виде кортежа - (x_distance, y_distance) в том же порядке
+    def get_pos(self):  
         pos_pegasus = (self.x_distance, self.y_distance)
         return pos_pegasus
 
-    def voice(self):    # печатает значение унаследованного атрибута sound.
+    def voice(self):   
         print(self.sound)
 
 pegasus1 = Pegasus()
