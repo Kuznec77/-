ea5d455b128dcd898ac55294a2a6207ad2266467
Kuznec77 <super.kuznec-77@yandex.ru>
2024-11-30 18:16:20 +0300

    Update module_6hard.py

--- module_6hard.py
+++ module_6hard.py
@@ -1,156 +1,115 @@
-import math
-
-from module_6_4 import circle1
-
+# Задание " Они все так похожи"
 
 class Figure:
     sides_count = 0
-    filled = True
 
-    def __init__(self, color):
-        if self.__is_valid_color(color):
-            self.__color = color
+    def __init__(self, color, *sides):
+
+        self.__color = color  # список цветов в формате RGB
+        self.__sides = sides  # Список сторон целые числа
+        #self.filled = False        #закрашенный, bool
 
     def get_color(self):
         return self.__color
 
-    def __is_valid_color(self, color):
-        for i in color:
-            if isinstance(i, int) and i>=0 and i<=255:
-                pass
-            else:
-                return False
-            if len(color)==3:
-                return True
+    def __is_valid_color(self, r, g, b):
 
-    def set_color(self, *color):
-        if self.__is_valid_color(color):
-            self.__color=color
+        if 0 < r <= 255 and 0 < g <= 255 and 0 < b <= 255:  # Цвет корректный
+            return r, g, b
+        else:
+            return self.__color  # Цвет некорректный
 
-    def set_sides(self, *new_sides):
-        if self.check_len(sides):
-            self.__sides=new_sides
+    def set_color(self, r, g, b):
+        new_color = self.__is_valid_color(r, g, b)
+        self.__color = list(new_color)
+        return self.__color
+
+    def __is_valid_sides(self, *new_sides):
+        for i in new_sides:
+            if i > 0:
+                if len(new_sides) == self.sides_count:
+                    return True
+
+                else:
+                    return False
 
     def get_sides(self):
         return self.__sides
 
     def __len__(self):
-        return sum(self.get_sides())
+        return sum(self.__sides)
 
-    def check_len(self,args):
-        if len(args)==self.sides_count:
-            return True
+    def set_sides(self, *new_sides):
+        for j in new_sides:
+            if j != self.__is_valid_sides(j):
+                self.__sides = list(new_sides)
+                return self.__sides
 
-        else:
-            return False
 
 class Circle(Figure):
-    sides_count = 1
+    sides_count = 1  #Список сторон целые числа
 
     def __init__(self, color, *sides):
-        super().__init__(color)
-        if self.check_len(sides):
-            self.__sides=sides
-        else:
-            self.__radius = self.get_radius()
-
-    def set_sides(self, *new_sides):
-        if self.check_len(new_sides):
-            self.__sides=new_sides
-
-    def get_sides(self):
-        return self.__sides
-    def get_radius(self):
-        return self.__sides[0]/(2*math.pi)
-    def get_square(self):
-        return math.pi*(self.get_radius()**2)
-
-class Triangle(Figure):
-    sides_count=3
-
-    def __init__(self, color, *sides):
-        super().__init__(color)
-        if self.check_len(sides):
-            self.__sides=sides
-        else:
-            self.__sides=(1,)*3
-
-    def get_sides(self):
-        return self.__sides
-
-    def set_sides(self, *new_sides):
-        if self.check_len(new_sides):
-            self.__sides=new_sides
+        super().__init__(color, *sides)
+        self.__sides = sides
+        self.__radius = self.__sides[0] / (2 * pi)
 
     def get_square(self):
-        sides=self.get_sides()
-        p=sum(sides)/2
-        s=abs((p*(p-sides[0])*(p-sides[1])*(p-sides[2])))
-        s=math.sqrt(s)
+        s = (self.__radius ** 2) * pi
         return s
+
 
 class Cube(Figure):
     sides_count = 12
+
     def __init__(self, color, *sides):
-        super().__init__(color)
-        if self.check_len(sides):
-            self.__sides=sides
-        elif len(sides)==1:
-            self.__sides=(sides[0],)*12
-        else:
-            self.__sides=(1,)*12
+        super().__init__(color, *sides)
+        self.__sides = [sides[0]] * self.sides_count  # переопределение __sides
+        print(self.__sides)
 
-    def set_sides(self, *new_sides):
-        if self.check_len(new_sides):
-            self.__sides=new_sides
-    def get_sides(self):
-        return self.__sides
     def get_volume(self):
-        return self.get_sides()[0]**3
+        V = self.__sides[0] ** 3
+        return V
 
-circlel = Circle((200, 200, 100), 10, 5)
+
+class Triangle(Figure):
+    sides_count = 3
+
+    def __init__(self, color, *sides):
+        super().__init__(color, *sides)
+        self.sides = sides
+        p = (self.sides[0] + self.sides[1] + self.sides[2]) / 2
+        self.__height = 2 * (sqrt(p * (p - self.sides[0]) * (p - self.sides[1]) * (p - self.sides[2]))) / self.sides[0]
+        #print(self.__height, "высота")
+
+    def get_square(self):
+        St = (self.__height * self.sides[0]) / 2
+        return St
+
+
+circle1 = Circle((200, 200, 100), 10)  # (Цвет, стороны)
 cube1 = Cube((222, 35, 130), 6)
+triangle1 = Triangle((155, 56, 30), 8, 3, 6)
 
-# Проверка на изменение цвета:
-circlel.get_color(55, 66, 77) # Изменится
-print(circlel.get_color())
-cube1.set_color(300, 70, 15) # Не изменится
+# Проверка на изменение цветов:
+circle1.set_color(55, 66, 77) # Изменится
+print(circle1.get_color())
+cube1.set_color(77, 500, 15) # Не изменится
 print(cube1.get_color())
 
 # Проверка на изменение сторон:
-cube1.set_sides(5, 3, 12, 4, 5) # Не изменится
+cube1.set_sides(5, 3, 12, 4, 5)  # Не изменится
 print(cube1.get_sides())
-circlel.set_sides(15) # Изменится
-print(circlel.get_sides())
+circle1.set_sides(15)  # Изменится
+print(circle1.get_sides())
 
 # Проверка периметра (круга), это и есть длина:
-print(len(circlel))
-
-# Проверка объёма (куба):
+print(len(circle1))
+#
+# # Проверка объёма (куба):
 print(cube1.get_volume())
 
-# Проверка площади круга
+
+# площадь
 print(circle1.get_square())
-
-# Проверка площади треугольника
-triangle1=Triangle((45,55,75),4,15)
-print(triangle1.get_sides())
 print(triangle1.get_square())
-
-cube3=Cube((255,280,10),4)
-
-print(cube3.get_sides())
-
-
-
-
-
-
-
-
-
-
-
-
-
-
