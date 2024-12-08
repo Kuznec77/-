9a8523ac5a2b1045095b880786f5e4d86cc22969
Sony <super.kuznec-77@yandex.ru>
2024-12-08 16:56:22 +0300

    08/12

---
+++ MODULES/module7/module_7_5.py
@@ -0,0 +1,16 @@
+import os
+import time
+
+directory = '.'
+
+for root, dirs, files in os.walk(directory):
+    for file in files:
+        filepath = os.path.join(root, file)
+        filetime = os.path.getmtime(filepath)
+        formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
+        filesize = os.path.getsize(filepath)
+        parent_dir = os.path.dirname(filepath)
+
+
+print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт,'
+      f' Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
