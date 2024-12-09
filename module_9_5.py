2b60d86714f5cc095b4d91e45c8689a6f4852681
Kuznec77 <super.kuznec-77@yandex.ru>
2024-12-09 22:48:36 +0300

    Update module_9_5.py

--- module_9_5.py
+++ module_9_5.py
@@ -1,40 +1,33 @@
 class StepValueError(ValueError):
     pass
 
-
 class Iterator():
 
     def __init__(self, start, stop, step = 1):
-        self.pointer = start
-        self.start, self.stop, self.step = start, stop, step
-        if step == 0:
-            raise StepValueError('Шаг не может быть равен 0')
+        if step != 0:
+            self.step = step
+        else:
+            raise StepValueError('шаг не может быть равен 0')
+        self.start = start
+        self.stop = stop
 
-    def __iter__(self):
+    def __iter__ (self):
         self.pointer = self.start
         return self
 
     def __next__(self):
-        pointer = self.pointer
-        if self.step > 0:
-            if self.pointer > self.stop:
-                raise StopIteration()
-            else:
-                self.pointer += self.step
-                return pointer
-        if self.step < 0:
-            if self.pointer < self.stop:
-                raise StopIteration()
-            else:
-                self.pointer += self.step
-                return pointer
+        num = self.pointer
+        if (self.step < 0 and self.pointer < self.stop) or (self.pointer > self.stop and self.step > 0):
+            raise StopIteration()
+        self.pointer += self.step
+        return num
 
 try:
     iter1 = Iterator(100, 200, 0)
     for i in iter1:
-        print(i, end='')
+        print(i, end=' ')
 except StepValueError:
-        print('Шаг указан неверно')
+    print('Шаг указан неверно')
 
 iter2 = Iterator(-5, 1)
 iter3 = Iterator(6, 15, 2)
@@ -42,14 +35,14 @@ iter4 = Iterator(5, 1, -1)
 iter5 = Iterator(10, 1)
 
 for i in iter2:
-    print(i, end='')
+    print(i, end=' ')
 print()
 for i in iter3:
-    print(i, end='')
+    print(i, end=' ')
 print()
 for i in iter4:
-    print(i, end='')
+    print(i, end=' ')
 print()
 for i in iter5:
-    print(i, end='')
+    print(i, end=' ')
 print()
